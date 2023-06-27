ce4053401b8d9e0694eb79354e29bff6df24442a
elainerecla <elaine.jrecla@gmail.com>
2023-06-27 20:16:16 +0800

    Release of Sunshine Project

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css"></link>
+	<title>Sunshine Playlist</title>
+</head>
+
+<body>
+	<div class="navbar">
+		<nav class="nav-left">
+			<ul>
+				<a href="#"><li>Home</li></a>
+				<a href="#"><li>Feed</li></a>
+				<a href="#"><li>Library</li></a>
+				<a href="#"><li>Link</li></a>
+			</ul>
+		</nav>
+		
+		<nav class="nav-right">
+			<ul>
+				<a href="#"><li>Login</li></a>
+				<a href="#"><li>Sign Up</li></a>
+			</ul>
+		</nav>
+	</div>
+	<div class="banner-img">
+	
+	</div>
+	
+	<div class="body-content">
+		<div class="body-left"></div>
+		<div class="body-right">
+			<div class="albums">
+				<div class="per-album">
+					<div class="artist">
+						<img src="imgs/album_cover/ryan jones.jpg" alt="ryan jones"/>
+					</div>
+					<strong>Ryan Jones</strong></br>
+					<i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</i>
+				</div>
+				
+				<div class="per-album">
+					<div class="artist">
+						<img src="imgs/album_cover/pink_floyd.jpg" alt="pink floyd"/>
+					</div>
+					<strong>Pink Floyd</strong></br>
+					<i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</i>
+				</div>
+				
+				<div class="per-album">
+					<div class="artist">
+						<img src="imgs/album_cover/1600w-1Nr6gsUndKw.jpg" alt="memories"/>
+					</div>
+					<strong>Memories</strong></br>
+					<i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</i>
+				</div>
+				
+				<div class="per-album">
+					<div class="artist">
+						<img src="imgs/album_cover/battle_songs.jpg" alt="battle songs"/>
+					</div>
+					<strong>Battle Songs</strong></br>
+					<i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</i>
+				</div>
+				
+				<div class="per-album">
+					<div class="artist">
+						<img src="imgs/album_cover/full_of_dreams.jpg" alt="full of dreams"/>
+					</div>
+					<strong>Full of Dreams</strong></br>
+					<i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</i>
+				</div>
+				
+				<div class="per-album">
+					<div class="artist">
+						<img src="imgs/album_cover/tyler_okonma.jpg" alt="tyler okonma"/>
+					</div>
+					<strong>Tyler Okonma</strong></br>
+					<i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</i>
+				</div>
+				
+				<div class="per-album">
+					<div class="artist">
+						<img src="imgs/album_cover/hot_in_the_winter.jpg" alt="hot in the winter"/>
+					</div>
+					<strong>Hot in the winter</strong></br>
+					<i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</i>
+				</div>
+				
+				<div class="per-album">
+					<div class="artist">
+						<img src="imgs/album_cover/linda_rodriguez.jpg" alt="linda rodriguez"/>
+					</div>
+					<strong>Linda Rodriguez</strong></br>
+					<i>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</i>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="footer">
+		<p>Copyright © 2023</p>
+	</div>
+</body>
+</html>
